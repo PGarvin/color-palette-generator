7e4924c400650984631f9e9a39cd1b92760b11ea
Patrick Garvin <patrickgarvin@patricks-mbp-2.attlocal.net>
2018-07-05 11:37:51 -0400

    Simplified the RGB conversion, added comments

--- main.js
+++ main.js
@@ -1,81 +1,68 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-const main = document.querySelectorAll('.main');
-const recipes = document.getElementById('recipes');
-const button = document.getElementById('generateButton');
-const dropdown = document.getElementById('dropdown');
-let currentNumber = dropdown.value;
+	const main = document.querySelectorAll('.main');
+	const recipes = document.getElementById('recipes');
+	const button = document.getElementById('generateButton');
+	const dropdown = document.getElementById('dropdown');
+	let currentNumber = dropdown.value;
+	const randomNumber = x => Math.floor(Math.random() * x);
+	const convertToHex = x => parseInt(x).toString(16);
 
+	//Generates a random color in both rgb and 6-digit hex code.
+	function randomColor() {
+		let r = randomNumber(256);
+		let g = randomNumber(256);
+		let b = randomNumber(256);	
+		let rgb = "rgba("+r+","+g+","+b+", 1)";
+		let hexcode = "#"+convertToHex(r)+convertToHex(g)+convertToHex(b);
+		return {"rgb":rgb, "hex":hexcode};
+	};
 
-function randomNumber(x) {
-	return Math.floor(Math.random() * x);
-};
+	//Creates palette and adds recipes to the recipe holder.
+	function palette(x) {
 
-function randomColor() {
-	let r = randomNumber(256);
-	let g = randomNumber(256);
-	let b = randomNumber(256);
-	return "rgba("+r+","+g+","+b+", 1)";
-};
-
-function rgb2hex(rgb){
- rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
- return (rgb && rgb.length === 4) ? "#" +
-  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
-  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
-  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
-};
-
-
-function palette(x) {
-
-	let colors = [];
-	for (let i=0; i<x; i++) {
-		colors.push(randomColor());
-	}
-	
-	for (let j=0; j<main.length; j++) {
-	
-		for (let i=0; i<100; i++) {
-			let bar = document.createElement('div');
-			bar.setAttribute('class','bars');
-			bar.setAttribute('id','bar'+i);
-			bar.style.background = colors[i % x];
-			main[j].appendChild(bar);		
-		}
-		
+		//Clears the recipes in the recipe holder.
 		recipes.innerHTML = '';
-		
+
+		//Creates new colors and stores them in an array.
+		let colors = [];
+		for (let i=0; i<x; i++) {
+			colors.push(randomColor());
+		}
+
+		//Appends the new recipes to the holder.
 		for (let i=0; i<colors.length; i++) {
 			let colorRecipe = document.createElement('div');
-			let colorRecipeContent = document.createTextNode(colors[i]+' // '+rgb2hex(colors[i]));
-			colorRecipe.style.color = colors[i];
+			let colorRecipeContent = document.createTextNode(colors[i].rgb+' // '+colors[i].hex);
+			colorRecipe.style.color = colors[i].rgb;
 			colorRecipe.appendChild(colorRecipeContent);
 			recipes.appendChild(colorRecipe);
 		}
-	}
+	
+		//Appends bars of those colors to both the vertical and horizontal holders.
+		for (let j=0; j<main.length; j++) {
+			main[j].innerHTML = "";
+			for (let i=0; i<100; i++) {
+				let bar = document.createElement('div');
+				bar.setAttribute('class','bars');
+				bar.style.background = colors[i % x].rgb;
+				main[j].appendChild(bar);		
+			}
+		}
+	};
 
-};
-
-
-button.addEventListener('click', function() {
-	for (let j=0; j<main.length; j++) {
-		main[j].innerHTML = "";
-	}
+	//Executes the function so there's a palette upon page load.
 	palette(currentNumber);
-});
 
-dropdown.addEventListener('change', function() {
-	currentNumber = dropdown.value;
-	for (let j=0; j<main.length; j++) {
-		main[j].innerHTML = "";
-	}
-	palette(currentNumber);
-});
+	//Generates new palette upon button click.
+	button.addEventListener('click', function() {
+		palette(currentNumber);
+	});
 
-palette(currentNumber);
-
-
-
+	//Generates new palette upon dropdown change.
+	dropdown.addEventListener('change', function() {
+		currentNumber = dropdown.value;
+		palette(currentNumber);
+	});
 
 });
